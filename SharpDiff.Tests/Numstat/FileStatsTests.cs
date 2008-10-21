bb72d7d0f592f6dd86d75c438819d123949f2529
James Gregory <james@jagregory.com>
2008-10-21 23:20:29 +0100

    Added multiline numstat parsing

--- SharpDiff.Tests/Numstat/FileStatsTests.cs
+++ SharpDiff.Tests/Numstat/FileStatsTests.cs
@@ -23,7 +23,7 @@ namespace SharpDiff.Tests.Numstat
         [Test]
         public void ParsesAdditionsAndSubtractionValues()
         {
-            var result = Parse<FileStats>("3\t8\tanotherFile.txt", x => x.FileStats);
+            var result = Parse<FileStats>("3\t8\tanotherFile.txt\r\n", x => x.FileStats);
 
             Assert.That(result.Additions, Is.EqualTo(3));
             Assert.That(result.Subtractions, Is.EqualTo(8));
@@ -40,14 +40,35 @@ namespace SharpDiff.Tests.Numstat
         [Test]
         public void ParsesFullFileLine()
         {
-            var result = Parse<FileStats>("3\t8\tanotherFile.txt", x => x.FileStats);
+            var result = Parse<FileStats>("3\t8\tanotherFile.txt\r\n", x => x.FileStats);
 
             Assert.That(result.Filename, Is.EqualTo("anotherFile.txt"));
+        }
+
+        [Test]
+        public void CanParseMultipleLines()
+        {
+            var result = ParseList<FileStats>(
+                "3\t8\tfile.txt\r\n" +
+                "5\t1\tanotherFile.txt\r\n", x => x.FullFile);
+
+            Assert.That(result.Count, Is.EqualTo(2));
+            Assert.That(result[0].Filename, Is.EqualTo("file.txt"));
+            Assert.That(result[0].Additions, Is.EqualTo(3));
+            Assert.That(result[0].Subtractions, Is.EqualTo(8));
+            Assert.That(result[1].Filename, Is.EqualTo("anotherFile.txt"));
+            Assert.That(result[1].Additions, Is.EqualTo(5));
+            Assert.That(result[1].Subtractions, Is.EqualTo(1));
         }
 
         protected T Parse<T>(string text, Func<GitNumstatParser, Rule<char>> ruleFetcher)
         {
             return Grammars.ParseWith(text, ruleFetcher).As<T>();
+        }
+
+        protected IList<T> ParseList<T>(string text, Func<GitNumstatParser, Rule<char>> ruleFetcher)
+        {
+            return new List<T>(Grammars.ParseWith(text, ruleFetcher).ToIEnumerable<T>());
         }
     }
 }
--- SharpDiff/Parsers/GitNumstatParser.cs
+++ SharpDiff/Parsers/GitNumstatParser.cs
@@ -5,6 +5,37 @@ namespace SharpDiff.Parsers
 {
     public class GitNumstatParser : Parser
     {
+        public virtual bool FullFile(OMetaStream<char> inputStream, out OMetaList<HostExpression> result, out OMetaStream <char> modifiedStream)
+        {
+            OMetaList<HostExpression> files = null;
+            modifiedStream = inputStream;
+            if(!MetaRules.Apply(
+                delegate(OMetaStream<char> inputStream2, out OMetaList<HostExpression> result2, out OMetaStream <char> modifiedStream2)
+                {
+                    modifiedStream2 = inputStream2;
+                    if(!MetaRules.Many1(
+                        delegate(OMetaStream<char> inputStream3, out OMetaList<HostExpression> result3, out OMetaStream <char> modifiedStream3)
+                        {
+                            modifiedStream3 = inputStream3;
+                            if(!MetaRules.Apply(FileStats, modifiedStream3, out result3, out modifiedStream3))
+                            {
+                                return MetaRules.Fail(out result3, out modifiedStream3);
+                            }
+                            return MetaRules.Success();
+                        }
+                    , modifiedStream2, out result2, out modifiedStream2))
+                    {
+                        return MetaRules.Fail(out result2, out modifiedStream2);
+                    }
+                    files = result2;
+                    result2 = ( files ).AsHostExpressionList();
+                    return MetaRules.Success();
+                }, modifiedStream, out result, out modifiedStream))
+            {
+                return MetaRules.Fail(out result, out modifiedStream);
+            }
+            return MetaRules.Success();
+        }
         public virtual bool FileStats(OMetaStream<char> inputStream, out OMetaList<HostExpression> result, out OMetaStream <char> modifiedStream)
         {
             OMetaList<HostExpression> lines = null;
@@ -28,6 +59,10 @@ namespace SharpDiff.Parsers
                         return MetaRules.Fail(out result2, out modifiedStream2);
                     }
                     name = result2;
+                    if(!MetaRules.Apply(NewLine, modifiedStream2, out result2, out modifiedStream2))
+                    {
+                        return MetaRules.Fail(out result2, out modifiedStream2);
+                    }
                     result2 = ( new FileStats(lines[0].As<int>(), lines[1].As<int>(), name.As<string>()) ).AsHostExpressionList();
                     return MetaRules.Success();
                 }, modifiedStream, out result, out modifiedStream))
@@ -111,6 +146,28 @@ namespace SharpDiff.Parsers
                     }
                     ext = result2;
                     result2 = ( name.As<string>() + "." + ext.As<string>() ).AsHostExpressionList();
+                    return MetaRules.Success();
+                }, modifiedStream, out result, out modifiedStream))
+            {
+                return MetaRules.Fail(out result, out modifiedStream);
+            }
+            return MetaRules.Success();
+        }
+        public virtual bool NewLine(OMetaStream<char> inputStream, out OMetaList<HostExpression> result, out OMetaStream <char> modifiedStream)
+        {
+            modifiedStream = inputStream;
+            if(!MetaRules.Apply(
+                delegate(OMetaStream<char> inputStream2, out OMetaList<HostExpression> result2, out OMetaStream <char> modifiedStream2)
+                {
+                    modifiedStream2 = inputStream2;
+                    if(!MetaRules.ApplyWithArgs(Exactly, modifiedStream2, out result2, out modifiedStream2, ("\r").AsHostExpressionList()))
+                    {
+                        return MetaRules.Fail(out result2, out modifiedStream2);
+                    }
+                    if(!MetaRules.ApplyWithArgs(Exactly, modifiedStream2, out result2, out modifiedStream2, ("\n").AsHostExpressionList()))
+                    {
+                        return MetaRules.Fail(out result2, out modifiedStream2);
+                    }
                     return MetaRules.Success();
                 }, modifiedStream, out result, out modifiedStream))
             {
